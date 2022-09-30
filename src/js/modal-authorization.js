cdea25e6fe6bef07a1ac59f68a2681dce5cdbf26
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-09-30 21:23:58 +0300

    модалка-регистрация

--- src/js/modal-authorization.js
+++ src/js/modal-authorization.js
@@ -1,8 +1,12 @@
 const refs = {
     authorizationBtn: document.querySelector('#headerAuthorization'),
     modalAuthorizationBackdrop: document.querySelector('.backdrop-modalAuthorization'),
-    modalCloseAuthorozation: document.querySelector('.authorization__close')
+    modalCloseAuthorozation: document.querySelector('.authorization__close'),
+    modalAuthorizationLink: document.querySelector('.authorization__link'),
+    modalRegistrationBackdrop: document.querySelector('.backdrop-modalRegistration'),
+    modalCloseRegistration: document.querySelector('.registration__close'),
 }
+console.log(refs.modalAuthorizationLink);
 refs.authorizationBtn.addEventListener('click', onAuthorizationBtnClick);
 refs.modalCloseAuthorozation.addEventListener('click', onModalCloseAuthorozationClick);
 
@@ -15,4 +19,22 @@ function onAuthorizationBtnClick(event) {
 function onModalCloseAuthorozationClick(event) {
      event.preventDefault()
    refs.modalAuthorizationBackdrop.classList.add('visually-hidden') 
+}
+
+
+
+// Модальное окно регистрации
+refs.modalAuthorizationLink.addEventListener('click', onModalAuthorizationLinkClick);
+
+function onModalAuthorizationLinkClick(event) {
+    event.preventDefault()
+    refs.modalAuthorizationBackdrop.classList.add('visually-hidden');
+    refs.modalRegistrationBackdrop.classList.remove('visually-hidden')
+}
+
+refs.modalCloseRegistration.addEventListener('click', onModalCloseRegistrationClick);
+
+function onModalCloseRegistrationClick(event) {
+    event.preventDefault();
+    refs.modalRegistrationBackdrop.classList.add('visually-hidden')
 }
