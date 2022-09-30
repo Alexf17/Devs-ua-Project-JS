54ccc4ff7c2dccafd0e8af09523eaa3ec902cc34
Alex <100074196+Pupsikk@users.noreply.github.com>
2022-10-01 00:24:09 +0300

    header
    fixed header full

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ import './js/inputCheck';
 import './js/modal-film-info';
 import './js/changeTheme';
 import './js/modal-team';
-import './js/modal-authorization'
+import './js/modal-authorization';
+import './js/library-main'
