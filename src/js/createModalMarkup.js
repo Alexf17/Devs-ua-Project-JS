9a09ccb629eb28b7b7f1878849dbf62aaea5ef64
Oleksii Chilibiyskiy <98961066+Alexf17@users.noreply.github.com>
2022-10-01 23:39:57 +0300

    Update createModalMarkup.js

--- src/js/createModalMarkup.js
+++ src/js/createModalMarkup.js
@@ -1,5 +1,8 @@
 // Объявляем функцию, которая создает разметку одного модального окна при нажатии на карточку фильма
 import img from '../images/filmWrap.jpg';
+import { isExistsQueueObjFilm, isExistsWatchedObjFilm } from './localstorageApp';
+import { localStorageData } from './localstorageApp';
+
 export function createModalMarkup(data) {
   // Делаем деструктуризацию полученных файлов с бэкэнда
   const {
@@ -13,7 +16,14 @@ export function createModalMarkup(data) {
     genres,
     id,
   } = data;
-
+  
+  
+  let text_btn_watch = '';
+  let text_btn_queue = '';
+  
+  isExistsWatchedObjFilm(id) ? text_btn_watch = 'remove' : text_btn_watch = 'add to watched';
+  isExistsQueueObjFilm(id) ? text_btn_queue = 'remove' : text_btn_queue = 'add to queue';
+  
   //   Выстаскиваем из полученного объекта названия жанров
   const genresList = genres.map(({ name }) => name).join(', ');
 
@@ -63,7 +73,7 @@ export function createModalMarkup(data) {
               class="button-list__button button-modal__orange"
               data-action="watched"
             >
-              Add to watched
+              ${text_btn_watch}
             </button>
           </li>
           <li class="button-list__item">
@@ -73,7 +83,7 @@ export function createModalMarkup(data) {
               class="button-list__button button-modal__white"
               data-action="queue"
             >
-              Add to queue
+              ${text_btn_queue}
             </button>
           </li>
         </ul>
