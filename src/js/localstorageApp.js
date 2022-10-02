e65b2bee4ca532c486719a229d5206e44458d2e4
Darymee <danieledaniele3005@gmail.com>
2022-10-02 16:25:41 +0200

    fix modal btn

--- src/js/localstorageApp.js
+++ src/js/localstorageApp.js
@@ -1,14 +1,15 @@
-import { refs } from "./refs";
+import { refs } from './refs';
 
 // data-action="queue"
 // data-action="watched"
 
 // создаю и инициализирую переменную для работы с localstorage
-export let localStorageData = JSON.parse(localStorage.getItem('localStorageData')) || {
+export let localStorageData = JSON.parse(
+  localStorage.getItem('localStorageData')
+) || {
   queueFilms: [],
   watchedFilms: [],
 };
-
 
 refs.infoFilmWrapEl.addEventListener('click', onInfoFilmWrapClick);
 
@@ -19,40 +20,39 @@ function onInfoFilmWrapClick(e) {
 
   // фильтр собития только на кнопку
   if (e.target.nodeName !== 'BUTTON') {
-    return
+    return;
   }
   // Get link on watchedBtn
   if (e.target.dataset.action === 'watched') {
     let watchedBtn = e.target;
-     // Get id btn watched
+    // Get id btn watched
     let idFilmWatched = +e.target.id;
 
     // console.log('Click on e.target.dataset.action watchedBTN', watchedBtn);
     // console.log('id кнопки :', idFilmWatched);
-    
+
     for (const filmObj of fetchDataArr) {
-        // console.log(filmObj.id);
-        if (filmObj.id === idFilmWatched) {
-          let filmWatchedObjAdd = filmObj;
-            // console.log(filmWatchedObjAdd);
-            // зSet watched velue to localsrorage
-          if (!isExistsWatchedObjFilm(idFilmWatched)) {
-            // console.log('сработало записывает');
-            localStorageData.watchedFilms.push(filmWatchedObjAdd);
-            localStorage.setItem('localStorageData', JSON.stringify(localStorageData));
-            // console.log('Длина localStorageData свойство watchedFilms: ', localStorageData.watchedFilms.length);
-            setStatusRemove(watchedBtn);
-
-          } else {
-            // console.log('уже есть не записываю', 'localStorageData.length свойство watchedFilms:', localStorageData.watchedFilms.length);
-            removeObjFilm(localStorageData.watchedFilms, idFilmWatched);
-            setStatusAddToWatched(watchedBtn);
-          }
-
+      // console.log(filmObj.id);
+      if (filmObj.id === idFilmWatched) {
+        let filmWatchedObjAdd = filmObj;
+        // console.log(filmWatchedObjAdd);
+        // зSet watched velue to localsrorage
+        if (!isExistsWatchedObjFilm(idFilmWatched)) {
+          // console.log('сработало записывает');
+          localStorageData.watchedFilms.push(filmWatchedObjAdd);
+          localStorage.setItem(
+            'localStorageData',
+            JSON.stringify(localStorageData)
+          );
+          // console.log('Длина localStorageData свойство watchedFilms: ', localStorageData.watchedFilms.length);
+          setStatusRemove(watchedBtn);
+        } else {
+          // console.log('уже есть не записываю', 'localStorageData.length свойство watchedFilms:', localStorageData.watchedFilms.length);
+          removeObjFilm(localStorageData.watchedFilms, idFilmWatched);
+          setStatusAddToWatched(watchedBtn);
         }
-      
       }
-  
+    }
   }
 
   // Get link to queueBtn
@@ -62,35 +62,34 @@ function onInfoFilmWrapClick(e) {
 
     // Get id btn queue
     let idFilmQueue = +e.target.id;
-     
+
     for (const filmObj of fetchDataArr) {
-        // console.log(filmObj.id);
-        if (filmObj.id === idFilmQueue) {
-          let filmQueueObjAdd = filmObj;
-            // console.log(filmQueueObjAdd);
-          
-            //Set queue velue to localsrorage
-          if (!isExistsQueueObjFilm(idFilmQueue)) {
-            // console.log('сработало записывает');
-            localStorageData.queueFilms.push(filmQueueObjAdd);
+      // console.log(filmObj.id);
+      if (filmObj.id === idFilmQueue) {
+        let filmQueueObjAdd = filmObj;
+        // console.log(filmQueueObjAdd);
 
-            localStorage.setItem('localStorageData', JSON.stringify(localStorageData));
-            // console.log('Длина localStorageData свойство queueFilms: ', localStorageData.queueFilms.length);
-            setStatusRemove(queueBtn)
-            // queueBtn.textContent = 'remove';
-          } else {
-            // console.log('уже есть не записываю', 'localStorageData.length свойство queueFilms:', localStorageData.queueFilms.length);
-            removeObjFilm(localStorageData.queueFilms, idFilmQueue);
-            setStatusAddToQueue(queueBtn);
-            // console.log(localStorageData.queueFilms);
-          }
+        //Set queue velue to localsrorage
+        if (!isExistsQueueObjFilm(idFilmQueue)) {
+          // console.log('сработало записывает');
+          localStorageData.queueFilms.push(filmQueueObjAdd);
 
+          localStorage.setItem(
+            'localStorageData',
+            JSON.stringify(localStorageData)
+          );
+          // console.log('Длина localStorageData свойство queueFilms: ', localStorageData.queueFilms.length);
+          setStatusRemove(queueBtn);
+          // queueBtn.textContent = 'remove';
+        } else {
+          // console.log('уже есть не записываю', 'localStorageData.length свойство queueFilms:', localStorageData.queueFilms.length);
+          removeObjFilm(localStorageData.queueFilms, idFilmQueue);
+          setStatusAddToQueue(queueBtn);
+          // console.log(localStorageData.queueFilms);
         }
-      
       }
-    
+    }
   }
-
 }
 
 export function isExistsWatchedObjFilm(id) {
@@ -122,21 +121,22 @@ export function isExistsQueueObjFilm(id) {
 function removeObjFilm(propArr, checkId) {
   for (let i = 0; i < propArr.length; i++) {
     if (propArr[i].id === checkId) {
-      
       propArr.splice(i, 1);
-
     }
   }
 }
 
 function setStatusRemove(btnRef) {
   btnRef.textContent = 'remove';
+  btnRef.classList.add('button-modal__checked');
 }
 
 function setStatusAddToQueue(btnRef) {
   btnRef.textContent = 'add to queue';
+  btnRef.classList.remove('button-modal__checked');
 }
 
 function setStatusAddToWatched(watchedBtnRef) {
   watchedBtnRef.textContent = 'add to watched';
+  watchedBtnRef.classList.remove('button-modal__checked');
 }
