bf8429ab94961c288d1c0dc2c9277e447295c258
Oleksii <achili3f17@gmail.com>
2022-11-13 19:19:46 +0200

    Update youTube.js

--- src/js/youTube.js
+++ src/js/youTube.js
@@ -5,7 +5,7 @@ import { renderFoo } from './renderMarkup';
 import { cleanerMarkup } from './cleanerMarkup';
 import { Notify } from 'notiflix';
 
-if (window.location.pathname === '/index.html') {
+if (window.location.pathname === '/Devs-ua-Project-JS/index.html') {
   refs.cardListEl.addEventListener('click', onYoutubeClick);
 }
 
